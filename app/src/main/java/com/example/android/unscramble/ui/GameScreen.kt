c0f98d006e6a5aba639af7b0a1b9fcbe52e6435c
jtavva <jtavva@google.com>
2023-02-01 13:05:37 -0800

    BOM and other updates

--- app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
@@ -19,9 +19,7 @@ import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
