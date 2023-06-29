b2734967f1c9f017b66069cd389533e7f169de3b
Android dev <androidedu@google.com>
2023-06-29 10:55:56 -0700

    Addressed feedback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,14 +70,15 @@ android {
 dependencies {
     implementation (platform("androidx.compose:compose-bom:2023.06.00"))
     implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation ("androidx.compose.material3:material3")
     implementation ("androidx.compose.ui:ui")
     implementation ("androidx.compose.ui:ui-graphics")
     implementation ("androidx.compose.ui:ui-tooling-preview")
-    implementation ("androidx.compose.material3:material3")
     implementation ("androidx.core:core-ktx:1.10.1")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    debugImplementation ("androidx.compose.ui:ui-tooling")
+
     debugImplementation ("androidx.compose.ui:ui-test-manifest")
+    debugImplementation ("androidx.compose.ui:ui-tooling")
     testImplementation ("junit:junit:4.13.2")
 }
