057001e4f5d6fbb17a554e3f89de56348f0f19c2
Android dev <androidedu@google.com>
2023-06-29 10:57:30 -0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-@file:Suppress("UnstableApiUsage")
-
 /*
 * Copyright (C) 2023 The Android Open Source Project
 *
@@ -15,6 +13,8 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+
+@file:Suppress("UnstableApiUsage")
 
 plugins {
     id("com.android.application")
