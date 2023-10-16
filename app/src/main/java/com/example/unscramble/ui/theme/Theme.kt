441245cce5bb9bd4b8facc53cd43df53a55fe698
Android Dev <testingilt567@gmail.com>
2023-10-16 08:51:51 -0700

    Feedback

--- app/src/main/java/com/example/unscramble/ui/theme/Theme.kt
+++ app/src/main/java/com/example/unscramble/ui/theme/Theme.kt
@@ -16,22 +16,15 @@
 
 package com.example.unscramble.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import android.view.View
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -96,7 +89,6 @@ private val DarkColors = darkColorScheme(
     outlineVariant = md_theme_dark_outlineVariant,
     scrim = md_theme_dark_scrim,
 )
-
 
 @Composable
 fun UnscrambleTheme(
