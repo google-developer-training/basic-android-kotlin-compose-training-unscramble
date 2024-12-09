24ada2fac3f83824baaa15f1c2fe04fa8286e9b5
Jose Alcérreca <jalc@google.com>
2024-12-09 14:06:56 +0000

    Remove compiler options too

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
