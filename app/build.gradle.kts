f6b0dc8a2c4c8cee65f867ef748c5a57e0eb7750
Jose Alcérreca <jalc@google.com>
2024-12-09 12:49:15 +0000

    Adds Compose compiler plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -19,4 +19,5 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("com.android.library") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
